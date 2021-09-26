5db22b1085b66f654bd838b983b6d6d44430569a
Saif-K-Saeed <saif.k.saeed@gmail.com>
2021-09-26 18:19:01 +0300

    lab-08

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Card from "react-bootstrap/Card";
 import axios from "axios";
+import Movies from "./components/Movies";
 import Weather from "./components/Weather";
-
 // ////////////////////////////////////
 
 class App extends React.Component {
@@ -11,10 +11,11 @@ class App extends React.Component {
     super(props);
     this.state = {
       dataForCity: {},
-      nameFromInput: "",
+      nameFromInput: '',
       showCard: false,
-      showWeather :false,
+      showWeather: false,
       weatherData: [],
+      movieInformation: []
     };
   }
 
@@ -22,64 +23,70 @@ class App extends React.Component {
     event.preventDefault();
 
     await this.setState({
-      nameFromInput: event.target.cityName.value,
+      nameFromInput: event.target.city.value,
     });
-    let url = `https://eu1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&q=${this.state.nameFromInput}&format=json`;
 
+    let url = `https://eu1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&q=${this.state.nameFromInput}&format=json`;
 
     let resData = await axios.get(url);
     this.setState({
       dataForCity: resData.data[0],
-
       showCard: true,
     });
-
-    let url2 = `${process.env.REACT_APP_SERVER}getWeatherInfo?nameCity=${this.state.nameFromInput}`;
-
-    let weatherCity = await axios.get(url2);
-
-    await this.setState({
+    this.renderWeather();
+    this.renderMovie();
+  }
+  renderWeather = async () => {
+    const cityName = this.state.nameFromInput.charAt(0).toUpperCase() + this.state.nameFromInput.slice(1);
+    let weatherUrl = `${process.env.REACT_APP_SERVER}getWeatherInfo?city=${cityName}`;
+    let weatherCity = await axios.get(weatherUrl);
+    this.setState({
       weatherData: weatherCity.data,
       showWeather: true
-    });
+    })
 
   };
+  renderMovie = async () => {
+    const cityName = this.state.nameFromInput.charAt(0).toUpperCase() + this.state.nameFromInput.slice(1);
+    let movieUrl = `${process.env.REACT_APP_SERVER}movies?city=${cityName}`;
+    let movieData = await axios.get(movieUrl)
+    console.log(movieData.data);
+    this.setState({
+      movieInformation: movieData.data,
+    })
+
+  }
 
   render() {
     return (
-      <>
+      <div>
         <h1>City Explorer</h1>
-
         <form onSubmit={this.getCityLocation}>
-          <input type="text" name="cityName" placeholder="type city name..." />
-
+          <input type="text" name="city" placeholder="type city name..." />
           <input type="submit" value="Explorer!" />
         </form>
-
         {this.state.showCard &&
           <Card style={{ width: "30rem" }}>
             <Card.Img
               variant="top"
               src={`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&center=${this.state.dataForCity.lat},${this.state.dataForCity.lon}&zoom=12`}
-              alt="{this.state.dataForCity.display_name}"/>
-              
+              alt="{this.state.dataForCity.display_name}" />
             <Card.Body>
+
               <Card.Title>{this.state.dataForCity.display_name}</Card.Title>
-
               <Card.Text>
-                latitude: {this.state.dataForCity.lat}&nbsp; 
+                latitude: {this.state.dataForCity.lat}&nbsp;
                 longitude:{"  "}{this.state.dataForCity.lon}
-
-                {this.state.weatherData.map(info => {
-                  return (
-                    <Weather weatherData={info} />
-                  )
-                })}
+                <Weather weather={this.state.weatherData}/>
+                <Movies movies={this.state.movieInformation} />
               </Card.Text>
             </Card.Body>
           </Card>
         }
-      </>
+
+
+
+      </div>
     );
   }
 }
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -3,21 +3,20 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Card from "react-bootstrap/Card";
 
 class Weather extends React.Component {
-  
+
   render() {
     return (
-      <>
-        <Card style={{ width: "30rem" }}>
-          <Card.Body>
-               
-                  <Card.Text>{this.props.weatherData.date}</Card.Text>
-                  <Card.Text>{this.props.weatherData.description}</Card.Text>
-               
-          </Card.Body>
-        </Card>
-      </>
+      <Card style={{ width: "30rem" }}>
+        {this.props.weather.map((value, inx) => {
+          return (
+            <ul key={inx}>
+              <li>{value.date}  </li>
+              <li>{value.description}</li>
+            </ul>
+          )
+        })}
+      </Card>
     );
   }
 }
-
 export default Weather;
